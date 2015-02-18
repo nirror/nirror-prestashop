d32e7f22181e1265ee425e74931c6a4f61c55704
antoine <antoine@T500>
2015-02-18 21:43:19 +0100

    Added Spanish translation. Kudos to Sophie for the translation.

---
+++ translations/es.php
@@ -0,0 +1,25 @@
+<?php
+
+global $_MODULE;
+$_MODULE = array();
+$_MODULE['<{nirror}prestashop>nirror_f9497496b1260127b5910f3fe2770ba0'] = 'Nirror';
+$_MODULE['<{nirror}prestashop>nirror_f6424d2e4523a756e0622f08f33ce018'] = 'Registra el tráfico de sus clientes. Permite verificar o consultar en tiempo real el tráfico para analizarlo.';
+$_MODULE['<{nirror}prestashop>nirror_bb8956c67b82c7444a80c6b2433dd8b4'] = '¿Desea desinstalar este módulo?';
+$_MODULE['<{nirror}prestashop>nirror_f4f70727dc34561dfde1a3c529b6205c'] = 'Ajustes';
+$_MODULE['<{nirror}prestashop>nirror_d8d20cdc7cecfde900c73e1886e12e2a'] = 'Identificador Nirror';
+$_MODULE['<{nirror}prestashop>nirror_23c650d24eebad3984c40b4b10b966e2'] = 'Pegar el identificador Nirror de esta página';
+$_MODULE['<{nirror}prestashop>nirror_38f909f36179054e1949fb2e1b92edee'] = 'Seguir los clientes';
+$_MODULE['<{nirror}prestashop>nirror_a8c65ba2db652e457fb4a9ac09ef633c'] = 'Añade la referencia del cliente y la dirección de correo electrónico al registro de la navegación. Sólo funciona cuando el cliente se ha identificado.';
+$_MODULE['<{nirror}prestashop>nirror_00d23a76e43b46dae9ec7aa9dcbebb32'] = 'Sí';
+$_MODULE['<{nirror}prestashop>nirror_b9f5c797ebbf55adccdd8539a65a0241'] = 'No';
+$_MODULE['<{nirror}prestashop>nirror_bc302a51ad300b4099ef240ccc048efe'] = 'Registro de los formularios';
+$_MODULE['<{nirror}prestashop>nirror_7b752708fb5c7e084ade8a4430816483'] = 'Se va a proceder al registro de la entrada de datos en los formularios.';
+$_MODULE['<{nirror}prestashop>nirror_22ac29fea6926bca03f9d7d19b66e5b0'] = 'Direcciones IP prohibidas';
+$_MODULE['<{nirror}prestashop>nirror_14eb7ac4226f10d1e7b5ac7eb01e1039'] = 'Introducir una dirección IP en cada línea. No se va a proceder al registro de la navegación de las direcciones IP prohibidas. Su dirección IP actual es ';
+$_MODULE['<{nirror}prestashop>nirror_c9cc8cce247e49bae79f15173ce97354'] = 'Registrar';
+$_MODULE['<{nirror}prestashop>nirror_2295a035ec6686494a1e82f8994bde02'] = 'Este módulo necesita una configuración.';
+$_MODULE['<{nirror}prestashop>nirror_0d4ee96f32f52eab5fb83ca33c5985da'] = 'Los ajustes han sido registrados.';
+$_MODULE['<{nirror}prestashop>nirror_ed162528562649456c145efc275f50fa'] = 'Se ha producido un error durante el registro de los ajustes.';
+$_MODULE['<{nirror}prestashop>nirror_b3973594176c94e047ebc069319be847'] = 'El identificador Nirror es incorrecto. Por favor, indique un identificador válido.';
+
+return $_MODULE;
