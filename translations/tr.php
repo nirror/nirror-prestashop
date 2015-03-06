6e68f367bc195e90e7b1ef36125f4627b3d13a43
antoine <antoine@T500>
2015-03-06 10:56:23 +0100

    TR translation updated.

--- translations/tr.php
+++ translations/tr.php
@@ -12,8 +12,6 @@ $_MODULE['<{nirror}prestashop>nirror_38f909f36179054e1949fb2e1b92edee'] = 'Müş
 $_MODULE['<{nirror}prestashop>nirror_a8c65ba2db652e457fb4a9ac09ef633c'] = 'Müşterinin numarasını ve email\'ini kayda ekle. Sadece müşteri tanımlandığında eklenir.';
 $_MODULE['<{nirror}prestashop>nirror_00d23a76e43b46dae9ec7aa9dcbebb32'] = 'Evet';
 $_MODULE['<{nirror}prestashop>nirror_b9f5c797ebbf55adccdd8539a65a0241'] = 'Hayır';
-$_MODULE['<{nirror}prestashop>nirror_bc302a51ad300b4099ef240ccc048efe'] = 'Formları kaydet';
-$_MODULE['<{nirror}prestashop>nirror_7b752708fb5c7e084ade8a4430816483'] = 'Formlarda yazılanlar kaydedilsin mi?';
 $_MODULE['<{nirror}prestashop>nirror_22ac29fea6926bca03f9d7d19b66e5b0'] = 'Engellenmiş IP adresleri';
 $_MODULE['<{nirror}prestashop>nirror_14eb7ac4226f10d1e7b5ac7eb01e1039'] = 'Satır başına bir IP adresi girin. Engellenmiş IP adreslerinin ziyaretleri kaydedilmeyecektir. Şu anki IP adresiniz ';
 $_MODULE['<{nirror}prestashop>nirror_c9cc8cce247e49bae79f15173ce97354'] = 'Kaydet';
@@ -21,5 +19,16 @@ $_MODULE['<{nirror}prestashop>nirror_2295a035ec6686494a1e82f8994bde02'] = 'Bu mo
 $_MODULE['<{nirror}prestashop>nirror_0d4ee96f32f52eab5fb83ca33c5985da'] = 'Ayarlar kaydedildi.';
 $_MODULE['<{nirror}prestashop>nirror_ed162528562649456c145efc275f50fa'] = 'Ayarlar kaydedilken bir hata oluştu.';
 $_MODULE['<{nirror}prestashop>nirror_b3973594176c94e047ebc069319be847'] = 'Nirror numarası geçersiz. Lütfen geçerli bir Nirror numarası giriniz.';
+$_MODULE['<{nirror}prestashop>nirror_e54dc5278c572df1baf2fec47b680cc2'] = 'Nirror uygulamasına giriş';
+$_MODULE['<{nirror}prestashop>nirror_34220e549c2542a9e41717d12f9dd5f5'] = 'Nirror kimlik doğrulama';
+$_MODULE['<{nirror}prestashop>nirror_d7a478866bf25b2539f4023c4664cb18'] = 'Prestashop için Nirror ayarları gerçekleştirmek için Nirror\'a bağlantı gerektiriyor.';
+$_MODULE['<{nirror}prestashop>nirror_bc302a51ad300b4099ef240ccc048efe'] = 'Formları kaydet';
+$_MODULE['<{nirror}prestashop>nirror_7b752708fb5c7e084ade8a4430816483'] = 'Formlarda klavye girdilerini kaydet';
+$_MODULE['<{nirror}prestashop>nirror_343cc1aa5426fc9da2bba2c81a4027cf'] = 'Nirror API\'ına bağlantı reddedildi. Tekrar deneyiniz.';
+$_MODULE['<{nirror}prestashop>nirror_dd397af432165b27b17d5101e1ec8c74'] = 'API\'ya bağlantı izni verildi.';
+$_MODULE['<{nirror}prestashop>nirror_dc71c352298c5b326d53fcbc68b04b69'] = 'Nirror\'da site (Id : %s) alan adıyla uyumlu. Bu mağaza için seçilmiştir.';
+$_MODULE['<{nirror}prestashop>nirror_d5fe1af516cd74c13826e81916ee58e2'] = '\'%s\' adında yeni bir site oluşturuldu. Modülün ayarları tamamlanmıştır.';
+$_MODULE['<{nirror}prestashop>nirror_a3b317fa007a7f6f422012306890d463'] = 'Nirror sitesi otomatik olarak kaydedilemedi. Modülü tekrar kurunuz.';
+$_MODULE['<{nirror}prestashop>nirror_0beeb4005678cf92fea9297a1d671129'] = 'Nirror API\'ına bağlantı süre aşımına uğradı. Bağlantı iznini tekrarlayınız.';
 
 return $_MODULE;
